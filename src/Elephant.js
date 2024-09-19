aad9febf023d82dfe075d5e558aba411ecafe345
Elephantatech <vivekm.mistry@gmail.com>
2024-09-19 13:51:30 -0400

    updated main

---
+++ src/Elephant.js
@@ -0,0 +1,17 @@
+// Elephant.js
+import React from 'react';
+import './Elephant.css';
+
+const Elephant = () => {
+  return (
+    <div className="elephant">
+      <div className="body"></div>
+      <div className="ear"></div>
+      <div className="eye"></div>
+      <div className="trunk"></div>
+      <div className="tail"></div>
+    </div>
+  );
+};
+
+export default Elephant;
